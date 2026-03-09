a146d89f6295a354549e879fa5556207ed921ab3
simon carmona toro <simoncarmonat@hotmail.com>
2026-03-09 14:09:34 -0500

    característica: agregar la estructura base del proyecto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace BibliotecandoAndo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Iniciando sistema...");
+        }
+    }
+}
