d74a6ab9d45d0913b4bb8c7ac5ae9f042bf624ec
simon carmona toro <simoncarmonat@hotmail.com>
2026-03-09 14:18:06 -0500

    característica: agregar submenú de gestión de usuarios

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace BibliotecandoAndo {
                 Console.Write("Opción: ");
                 switch (Console.ReadLine()) {
                     case "1": ShowBooksMenu(); break;
+                    case "2": ShowUsersMenu(); break;
                     case "6": exit = true; break;
                 }
             }
@@ -21,6 +22,12 @@ namespace BibliotecandoAndo {
             Console.Clear();
             Console.WriteLine("--- MENÚ LIBROS ---");
             Console.WriteLine("1. Registrar libro\n2. Listar libros\n3. Ver detalle\n4. Actualizar libro\n5. Eliminar libro\n6. Volver");
+            Pause();
+        }
+        static void ShowUsersMenu() {
+            Console.Clear();
+            Console.WriteLine("--- MENÚ USUARIOS ---");
+            Console.WriteLine("1. Registrar usuario\n2. Listar usuarios\n3. Ver detalle\n4. Actualizar usuario\n5. Eliminar usuario\n6. Volver");
             Pause();
         }
 
